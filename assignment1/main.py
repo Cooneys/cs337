91af199a12ef807d827e4f0db666bb78d6ab9944
Sam Cooney <cooneys@oregonstate.edu>
2019-04-18 23:17:22 -0700

    Add state class and placeholders for functions

--- assignment1/main.py
+++ assignment1/main.py
@@ -3,6 +3,69 @@ import os
 
 
 ######################################
+## Class: State
+## input: list river
+######################################
+
+class State():
+    def __init__(self, river):
+        leftBank = river[0].split(",")
+        rightBank = river[1].split(",")
+        self.lbChickens = leftBank[0]
+        self.lbWolves = leftBank[1]
+        self.lbBoat = leftBank[2]
+        self.rbChickens = rightBank[0]
+        self.rbWolves = rightBank[1]
+        self.rbBoat = rightBank[2]
+
+    def printState(self):
+        print("Left Bank: %s chickens, %s wolves, %s boat"%(self.lbChickens, self.lbWolves, self.lbBoat), end=" - ")
+        print("Right Bank: %s chickens, %s wolves, %s boat"%(self.rbChickens, self.rbWolves, self.rbBoat))
+
+
+
+######################################
+## Mode: bfs
+## Input: string startRiver, string goalRiver
+## Output: list outputRiver
+######################################
+
+def runBfs(startRiver, goalRiver):
+    initial = State(startRiver)
+    initial.printState()
+ 
+
+######################################
+## Mode: dfs
+## Input: string startRiver, string goalRiver
+## Output: list outputRiver
+######################################
+
+def runDBfs(startRiver, goalRiver):
+   return 
+
+
+######################################
+## Mode: iddfs
+## Input: string startRiver, string goalRiver
+## Output: list outputRiver
+######################################
+
+def runIddfs(startRiver, goalRiver):
+   return 
+
+
+######################################
+## Mode: astar
+## Input: string startRiver, string goalRiver
+## Output: list outputRiver
+######################################
+
+def runAstar(startRiver, goalRiver):
+   return 
+
+
+######################################
 ## Input: string filename, list river
 ## Output: bool Success
 ######################################
@@ -55,19 +118,35 @@ def readInputFile(fileName):
 ## Output: None
 ######################################
 def start():
-    river = [[0,0,0]]
     if (len(sys.argv) != 5):
         print("You entered %i"%(len(sys.argv)) + " argument(s)")
         print("Invalid number of arguments. Please enter in the following format:\n< initial state file > < goal state file > < mode > < output file >")
         exit()
     startRiver = createRiverList(str(sys.argv[1]))
     goalRiver = createRiverList(str(sys.argv[2]))
+    mode = str(sys.argv[3])
+
+    if mode == "bfs":
+        runBfs(startRiver, goalRiver)
+    elif mode == "dfs":
+        print("nice")
+
+    elif mode == "iddfs":
+        print("nice")
+
+    elif mode == "astar":
+        print("nice")
+
+    else:
+        print("Uncompatable mode. Good bye.")
+        exit()
 
     success = createOutputFile(str(sys.argv[4]), goalRiver)
     if success:
         print("%s successfully created. Good bye."%(str(sys.argv[4])))
     else:
         print("%s failed to be created. Good bye."%(str(sys.argv[4])))
+        exit()
 
 
 
